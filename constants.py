0fa5336b0cf79e7968597e70e598ef59fad3d577
Matthew Liu <matthewliu@gmail.com>
2012-03-25 15:36:54 -0700

    Adding .gitignore file

--- constants.py
+++ constants.py
@@ -1,10 +1,2 @@
-BASE_PATH = '/var/www/vmserver/meaninglesscodename/'
-
-ROOT_URLCONF_DIR = 'meaninglesscodename'
-
-DB_ENGINE = 'django.db.backends.mysql'
-DB_NAME = 'meaninglesscodename'
-DB_USER = 'root'
-DB_PASSWORD = 'password'
-DB_HOST = ''
-DB_PORT = '3306'
+from constants_base import *
+from constants_override import *
---
+++ constants_base.py
@@ -0,0 +1,10 @@
+BASE_PATH = '/var/www/vmserver/meaninglesscodename/'
+
+ROOT_URLCONF_DIR = 'meaninglesscodename'
+
+DB_ENGINE = 'django.db.backends.mysql'
+DB_NAME = 'meaninglesscodename'
+DB_USER = 'root'
+DB_PASSWORD = 'password'
+DB_HOST = ''
+DB_PORT = '3306'
