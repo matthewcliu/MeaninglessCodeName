56bb2831b2f0c0ac208cc0c402b0473d5a611005
Matthew Liu <matthewliu@gmail.com>
2012-05-13 17:20:42 -0700

    Super hacky map marker complete. Need to fix JSON encoding.

--- web/controllers/IndexController.py
+++ web/controllers/IndexController.py
@@ -5,6 +5,8 @@ from meaninglesscodename import settings
 from django.template.defaultfilters import slugify
 from django.template import Context, loader
 from datetime import *
+from django.core import serializers
+import simplejson
 import re
 
 from meaninglesscodename import constants
@@ -41,8 +43,11 @@ class IndexController(BaseController):
                 data_request_obj = data_request_form.DataRequestForm.form_to_obj_storage(form)                                                
                 data_response_obj = data_request_handler.handle_request(data_request_obj)
                 entity_instances_to_template = data_response_obj.entity_instance_list
+
+                json_serializer = serializers.get_serializer("json")()
+                json_entity_instances = json_serializer.serialize(entity_instances_to_template, ensure_ascii=False)
                                 
-                params = { 'entity_instances_to_template': entity_instances_to_template, 'form':form }
+                params = { 'entity_instances_to_template': entity_instances_to_template, 'json_entity_instances': json_entity_instances, 'form':form }
                 
                 return self.render_to_response('listing.html', params) # Redirect after POST
         else:
@@ -55,10 +60,13 @@ class IndexController(BaseController):
     def render_testmap(self):
 
         data_response_obj = data_request.DataResponse()
-        data_response_obj.entity_instance_list = EntityInstance.objects.filter(latitude = '377250')
+        data_response_obj.entity_instance_list = EntityInstance.objects.filter(latitude__in = ('377250', '367550', '387750', '377750'))
         entity_instances_to_template = data_response_obj.entity_instance_list
+        
+        json_serializer = serializers.get_serializer("json")()
+        json_entity_instances = json_serializer.serialize(entity_instances_to_template, ensure_ascii=False)
 
-        params = { 'entity_instances_to_template': entity_instances_to_template }
+        params = { 'entity_instances_to_template': entity_instances_to_template, 'json_entity_instances': json_entity_instances }
 
         return self.render_to_response('testmap.html', params )
 
--- web/static/js/testmap.js
+++ web/static/js/testmap.js
@@ -2,6 +2,7 @@ var meaninglesscodename = meaninglesscodename || {};
 meaninglesscodename.testmap = meaninglesscodename.testmap || {};
 
 function initialize() {
+
     var myLatlng = new google.maps.LatLng(37.7750,-122.4183);
     var myOptions = {
     zoom: 8,
@@ -11,23 +12,30 @@ function initialize() {
 
     var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
-    var location = new google.maps.LatLng(latitude, longitude);
+    for (var i = 0; i < json_object_for_map.locations.length; i++) {
 
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map
-    });
+        latitude = json_object_for_map.locations[i].fields.latitude/10000;
+        longitude = json_object_for_map.locations[i].fields.longitude/10000;
+        entity_node = json_object_for_map.locations[i].fields.entitynode;
+        time_instance = json_object_for_map.locations[i].fields.time_instance;
 
-    var infowindow = new google.maps.InfoWindow({
-        content: name + time_instance,
-        size: new google.maps.Size(50,50)
-    });
+        var location = new google.maps.LatLng(latitude, longitude);
 
-    attachInfoWindows(marker, infowindow);
-
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+                        
+        var infowindow = new google.maps.InfoWindow({
+            content: entity_node + " " + time_instance,
+            size: new google.maps.Size(50,50)
+        });
+        
+        attachInfoWindows(map, marker, infowindow);        
+    } 
 }
 
-function attachInfoWindows(marker, infowindow){
+function attachInfoWindows(map, marker, infowindow){
     google.maps.event.addListener(marker, 'click', function(){
         infowindow.open(map, marker); 
     });
